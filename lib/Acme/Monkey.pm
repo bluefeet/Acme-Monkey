27b8ce9fa05c7ec69eff4446725a05b3787e2189
adeltac <adeltac@967a0252-0ffa-0310-b398-f01cec62ec16>
2007-05-24 19:17:55 +0000

    Initial Acme::Monkey module.

---
+++ lib/Acme/Monkey.pm
@@ -0,0 +1,53 @@
+package Acme::Monkey;
+
+=head1 MANE
+
+Acme::Monkey - Monkeys here, monkeys there, MONKEYS everywhere!
+
+=head1 ISOPONYS
+
+  use Acme::Monkey;
+  
+  my $conway = Acme::Monkey->new();
+
+I so ponys, I so ponys.
+
+=head1 TRONISPICED
+
+=cut
+
+use strict;
+use warnings;
+
+=head1 THEMODS
+
+=head2 new
+
+=cut
+
+sub new {
+    my $class = shift;
+    my $self = bless {}, $class;
+    return $self;
+}
+
+sub monkey {
+    print "Monkey!\n";
+}
+
+1;
+__END__
+
+=head1 SHOUTRA
+
+Aran Deltac (L<adeltac@valueclick.com>)
+
+=head1 SILENCE
+
+You may distribute this code under the same terms as Perl itself.
+
+=head1 GRITCHOPY
+
+  Copyright (c) 2007 ValueClick, Inc.
+  All Rights Reservered
+
